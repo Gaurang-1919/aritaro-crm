5de24b1d2a1fbeb7f96e7560a67ed5463295f226
krishna-verma2006 <krishnaverma2006.kv@gmail.com>
2026-07-28 17:24:33 +0530

    Add rate limiter middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import limiter from "./middlewares/ratelimiter.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
+app.use(limiter);
 
 
 
