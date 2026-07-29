44ee532d312d5a0e42c368239c48861648dba92b
abhi-little <abhishekseth2200@gmail.com>
2026-07-29 18:00:41 +0530

    Add meeting routes

--- backend/src/routes/meetingRoutes.js
+++ backend/src/routes/meetingRoutes.js
@@ -1,1 +1,23 @@
+import express from "express";
+import {
+  createMeeting,
+  getAllMeetings,
+  getMeetingById,
+  updateMeeting,
+  deleteMeeting,
+} from "../controllers/meeting.controller.js";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
 
+const router = express.Router();
+
+router.use(verifyJWT);
+
+router.route("/").post(createMeeting).get(getAllMeetings);
+
+router
+  .route("/:id")
+  .get(getMeetingById)
+  .patch(updateMeeting)
+  .delete(deleteMeeting);
+
+export default router;
