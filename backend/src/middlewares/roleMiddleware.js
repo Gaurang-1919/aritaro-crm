233a2118a0758099826ab99e0a74af22dddbfc80
abhi-little <abhishekseth2200@gmail.com>
2026-07-30 11:28:27 +0530

    Implement role-based authorization middleware
    Add role-based authorization middleware to restrict access.

--- backend/src/middlewares/roleMiddleware.js
+++ backend/src/middlewares/roleMiddleware.js
@@ -1,1 +1,15 @@
+import apierror from "../utils/apierror.js";
+
+const authorize = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return next(
+        new apierror(403, "You do not have permission to perform this action")
+      );
+    }
+    next();
+  };
+};
+
+export { authorize };
 
