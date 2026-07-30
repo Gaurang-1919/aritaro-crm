7a7673ce56883854345bcae2ca7469c400b33808
abhi-little <abhishekseth2200@gmail.com>
2026-07-30 11:12:32 +0530

    Refactor export statement for Notification model

--- backend/src/models/Notification.models.js
+++ backend/src/models/Notification.models.js
@@ -30,4 +30,5 @@ const notificationSchema = new mongoose.Schema(
   }
 );
 
-export default Notification=mongoose.model("Notification", notificationSchema);
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
